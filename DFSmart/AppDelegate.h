72233ad3203f9e3b168e801d92a903e1ac564e8a
apple <lzjapp@sina.com>
2018-05-17 23:02:15 +0800

    生成秘钥、本地git初始化

---
+++ DFSmart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DFSmart
+//
+//  Created by apple on 2018/5/17.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios,'9.0.0'
+inhibit_all_warnings!
+source 'https://github.com/Cocoapods/Specs.git'
+target 'DFSmart' do
+
+pod 'AFNetworking','~>3.1.0'
+pod 'JSONModel','~>1.7.0'
+pod 'SVProgressHUD','~>2.2.2'
+pod 'GVUserDefaults','~>1.0.2'
+pod 'MJRefresh','~>3.1.14.1'
+
+end
+
